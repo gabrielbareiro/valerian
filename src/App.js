add7e5595aae99666d57873ec75e5554ed2fa763
gabrielbareiro <minimalposadas@gmail.com>
2021-08-03 23:41:09 -0300

    creo el componente ItemListContainer

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import './App.css';
 import NavBar from './components/NavBar.js'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/js/dist/dropdown.js'
+import ItemListContainer from './components/ItemListContainer';
 
 function App() {
   return (
     <div className="App">
       <header className="container">
         <div>Valerian tienda virtual</div>
-        <NavBar/>
+        <NavBar />
       </header>
+      <main>
+        <ItemListContainer />
+      </main>
     </div>
+    
   );
 }
 
---
+++ src/components/ItemListContainer.js
@@ -0,0 +1,3 @@
+export default function ItemListContainer () {
+    return <div>catalogo</div>
+}
